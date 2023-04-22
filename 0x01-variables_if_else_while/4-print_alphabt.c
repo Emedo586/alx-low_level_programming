63385a598d6428f76fa4c57cfd7d5432fc13922e
emedo chiamaka <emedopeace99@gmai.com>
2023-04-21 20:45:45 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print alphabets in q and e'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+	while (n <= 122)
+	{
+	if (n == 101 || n == 113)
+	{
+		n++;
+		continue;
+	}
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
