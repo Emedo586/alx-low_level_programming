2b938725ce59ba80e1b276938976ae19a4bf6bfa
emedo chiamaka <emedopeace99@gmai.com>
2023-04-22 07:31:04 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'prints all alphabets in reverse'
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 92)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
