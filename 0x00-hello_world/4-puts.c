53ad379a794d00095d78100674ca3d925fe854bc
emedo chiamaka <emedopeace99@gmai.com>
2023-04-22 17:11:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * return: always 0{success}
+ * main - Entry point
+ * Description:  'Prints out a string to stdout.'
+ * Return: always 0 (success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
