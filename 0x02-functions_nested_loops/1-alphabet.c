e6b5dcd8c7c79452f83a188241e878fe5599204b
emedo chiamaka <emedopeace99@gmai.com>
2023-04-25 04:58:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * main - Entry point
+  * Description: ' prints the alphabet, in lowercase'
+  * Return: void
+  */
+void print_alphabet(void)
+{
+
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
