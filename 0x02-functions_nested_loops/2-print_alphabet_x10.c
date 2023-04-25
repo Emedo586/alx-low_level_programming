58d2bedaa1aa4f03a2a029cd4084bf3d20add5c3
emedo chiamaka <emedopeace99@gmai.com>
2023-04-25 10:57:20 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * print_alphabet_x10 - 'prints 10 times the alphabet in lowercase'
-  * Return: void
+  * Return: always 0
   *
   */
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 int i;
 int j;
 
-for (j = 1; i <= 10; i++)
+for (i = 1; i <= 10; i++)
 {
 for  (j = 97; j <= 122; j++)
 {
