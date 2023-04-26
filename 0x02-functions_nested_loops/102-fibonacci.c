415b637ad81be5c2b181b59f39d2df4a35f769e3
emedo chiamaka <emedopeace99@gmai.com>
2023-04-26 16:51:11 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
   * main - Entry point
-  * Description: 'prints the first 50 Fibonacci numbers'
+  *
+  * Description: 'prints the first 50 Fibonacci numbers,starting with 1 and 2'
   *
   * Return ; Always 0
   */
@@ -20,6 +21,7 @@ if (count == 49)
 printf("\n");
 else
 printf(",");
+printf(" ");
 }
 return (0);
 }
