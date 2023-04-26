4c4f2afb947ce59d2ae3f0637ca4137371b9d872
emedo chiamaka <emedopeace99@gmai.com>
2023-04-25 19:29:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,11 @@ void print_to_98(int n)
 int i;
 if  (n > 98)
 for (i = n; i > 98; i--)
-printf("%d, " , i);
+printf("%d, ", i);
 else
 {
 for (i = n; i < 98; i++)
-printf("%d, " , i);
+printf("%d, ", i);
 }
 printf("98\n");
 }
