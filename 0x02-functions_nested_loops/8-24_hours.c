4eed14b2e881dd4779ed57b8a25863e90152d9b6
emedo chiamaka <emedopeace99@gmai.com>
2023-04-25 18:32:36 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,22 @@
 #include"main.h"
 /**
-  * jack_bauer -  prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+  * jack_bauer -  prints every minute of the day of Jack Bauer
   *
   * Return: no return
   */
 void jack_bauer(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 2; a++)
+int min, hr;
+for (hr = 0; hr <= 23; hr++)
 {
-for (b = 0; b <= 9; b++)
+for (min = 0; min <= 59; min++)
 {
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-if (a <= 2 && b >= 4)
-break;
-_putchar(a + 48);
-_putchar(b + 48);
-_putchar(58);
-_putchar(c + 48);
-_putchar(d + 48);
+_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
+_putchar(':');
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
 _putchar('\n');
-}
-}
 }
 }
 }
