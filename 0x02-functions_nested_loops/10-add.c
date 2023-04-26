dbe3aea7e4a7720a9e746aea511de9fe37ba73dc
emedo chiamaka <emedopeace99@gmai.com>
2023-04-25 18:56:37 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * add - function that adds two integer and returns result
+  *
+  * @a: first input
+  * @b: second input
+  *
+  * Return: sum of a and b
+  */
+int add(int a, int b)
+{
+int sum = a + b;
+
+return (sum);
+}
