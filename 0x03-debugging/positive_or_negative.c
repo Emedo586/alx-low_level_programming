3e9cf7f5532cb1cf50cb38812d104053d321982d
emedo chiamaka <emedopeace99@gmai.com>
2023-04-27 15:56:25 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *main - Determine if a number is positive or negative or zero
+  *
+  * Return: void
+  */
+void positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else
+{
+printf("%d is zero\n", i);
+}
+}
