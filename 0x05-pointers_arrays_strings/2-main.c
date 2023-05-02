8a471eff583ddaf090ac70c40954283e0b593a54
emedo chiamaka <emedopeace99@gmai.com>
2023-05-02 08:56:35 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -5,17 +5,14 @@
  * main - check the code
  *
  * Return: Always 0.
- *
  */
 int main(void)
 {
-int a = 98;
-int b = 42;
+char *str;
+int len;
 
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
+str = "My first strlen!";
+len = _strlen(str);
+printf("%d\n", len);
 return (0);
 }
--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -5,17 +5,13 @@
  * main - check the code
  *
  * Return: Always 0.
- *
  */
 int main(void)
 {
-int a = 98;
-int b = 42;
+char s[10] = "My School";
 
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
+printf("%s\n", s);
+rev_string(s);
+printf("%s\n", s);
 return (0);
 }
