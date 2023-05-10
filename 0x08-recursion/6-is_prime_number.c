1393920bf782b1975074d50e1035fa1fd4e2ac94
emedo chiamaka <emedopeace99@gmai.com>
2023-05-10 10:20:11 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,7 +21,7 @@ return (check_prime(n, 2));
  */
 int check_prime(int n, int othrn)
 {
-if (othrn >=n && n > 1)
+if (othrn >= n && n > 1)
 return (1);
 else if (n % othrn == 0 || n <= 1)
 return (0);
