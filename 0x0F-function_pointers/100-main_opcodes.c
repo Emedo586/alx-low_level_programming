9493072a49e0a0d8dc3533febe0480910d9615f1
emedo chiamaka <emedopeace99@gmai.com>
2023-05-24 11:09:09 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,30 +8,16 @@
  * @argv: argument vector
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int bytes, i;
-char *arr;
+char *p = (char *)main;
+int b;
 if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-bytes = atoi(argv[1]);
-if (bytes < 0)
-{
-printf("Error\n");
-exit(2);
-}
-arr = (char *)main;
-for (i = 0; i < bytes; i++)
-{
-if (i == bytes - 1)
-{
-printf("%02hhx\n", arr[i]);
-break;
-}
-printf("%02hhx", arr[i]);
-}
+printf("Error\n"), exit(1);
+b = atoi(argv[1]);
+if (b < 0)
+printf("Error\n"), exit(2);
+while (b--)
+printf("%02hhx%s", *p++, b ? " " : "\n");
 return (0);
 }
