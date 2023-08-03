7af5dfcefcf28b6aeeee2c514c8ab44c88222eb6
Emedo586 <emedopeace99@gmail.com>
2023-08-03 15:27:37 -0700

    silent words

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int j = 0;
-while (*b)
-{
-if (*b != '0' && *b != '1')
-return (0);
-j = j * 2 + (*b++ - '0');
-}
-return (j);
-{
+int j;
+unsigned int num = 0;
 if (!b)
 return (0);
+for (j = 0; b[j]; j++)
+{
+if (b[j] != '0' && b[j] != '1')
+return (0);
+num = num * 2 + (b[j] - '0');
 }
+return (num);
 }
