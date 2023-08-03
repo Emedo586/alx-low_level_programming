64b721d193d2329da5dcc1df050d9308288cbf0a
Emedo586 <emedopeace99@gmail.com>
2023-08-03 14:40:32 -0700

    little minds

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  flip_bits - returns the number of bits you would 
+ *  flip_bits - returns the number of bits you would
  *  need to flip to get from one number to another.
  *
  *  @n: first number
