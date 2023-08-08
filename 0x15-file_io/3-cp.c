851dc90510d8667c8981b0ad5887b4aeba412e5f
Emedo586 <emedopeace99@gmail.com>
2023-08-07 17:10:31 -0700

    dinner

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,18 +5,17 @@
 
 /**
  * main - program that copies the content of a file to another file.
- * @ac: argument count.
- * @av: argument vector.
+ * @ac: the argument count.
+ * @av: the argument vector.
  *
- * Return: 1 on success -1 on failure.
+ * Return: 0 on success.
  *
  */
 int main(int ac, char **av)
 {
-int fd_from = 0;
-int fd_to = 0;
+int fd_0, fd_1;
+int res_0, res_1;
 char *buffer;
-ssize_t fp;
 
 if (ac != 3)
 {
@@ -24,41 +23,82 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 }
 buffer = malloc(BUF_SIZE * sizeof(char));
 if (!buffer)
+{
 return (0);
+}
 
-fd_from = open(av[1], O_RDONLY);
-if (fd_from == -1)
+fd_1 = open(av[1], O_RDONLY);
+/*error_98(fd_1, buffer, av[1]);*/
+fd_0 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, PERMISSIONS, 0664);
+/*error_99(fd_0, buffer, av[2]);*/
+do {
+res_0 = read(fd_1, buffer, BUF_SIZE);
+if (res_0 == 0)
+break;
+/*error_98(res_0, buffer, av[1])*/
+res_1 = write(fd_0, buffer, res_0);
+/*error_99(res_1, buffer, av[2])*/
+} while (res_1 >= BUF_SIZE);
+res_0 = close(fd_0);
+/*error_100(res_0, buffer)*/
+res_0 = close(fd_1);
+/*error_100(res_0, buffer);*/
+free(buffer);
+return (0);
+}
+
+/**
+ * error_98 - checks for error 98.
+ * @fd_0: value to check.
+ * @av: the argument vector.
+ * @buffer: the buffer.
+ *
+ *
+ */
+void error_98(int fd_0, char *buffer, char *av)
+{
+if (fd_0 < 0)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s\n",
-av[1]);
+av);
 free(buffer);
 exit(98);
 }
-fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS, 0664);
-if (fd_to == -1)
+}
+
+/**
+ * error_99 - checks for error 99.
+ * @fd_0: value to check.
+ * @av: the argument vector.
+ * @buffer: the buffer.
+ *
+ *
+ */
+void error_99(int fd_0, char *buffer, char *av)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", av[2]);
+if (fd_0 < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write from file NAME_OF_THE_FILE %s\n",
+av);
 free(buffer);
 exit(99);
 }
-while ((fp = read(fd_from, buffer, BUF_SIZE)) > 0)
-if (write(fd_to, buffer, fp) != fp)
-dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s\n", av[2]);
-free(buffer);
-exit(99);
-if (fp == -1)
-dprintf(STDERR_FILENO, "Error: Can't read to NAME_OF_THE_FILE %s\n", av[1]);
-free(buffer);
-exit(98);
-fd_from = close(fd_from);
-fd_to = close(fd_to);
-if (fd_from)
-dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", fd_from);
-free(buffer);
-exit(100);
-if (fd_to)
-dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", fd_from);
+}
+
+/**
+ * error_100 - checks for error 100.
+ * @fd_0: value to check.
+ *
+ * @buffer: the buffer.
+ *
+ *
+ */
+void error_100(int fd_0, char *buffer)
+{
+if (fd_0 < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", fd_0);
 free(buffer);
 exit(100);
-return (EXIT_SUCCESS);
+}
 }
