f61afbecc7a67aca6a4f03f44eb1be273fa3987b
Emedo586 <emedopeace99@gmail.com>
2023-09-14 18:31:36 -0700

    deep down

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * get_dlistint_at_index - Locates a node in a dlistint_t list.
+ * get_dnodeint_at_index - Locates a node in a dlistint_t list.
  * @head: pointer to the head of the dlistint list.
  * @index: the node  to locate
  * Return: NULL otherwise the address of the node.
  */
-dlistint_t *get_dlistint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 for (; index != 0; index--)
 {
